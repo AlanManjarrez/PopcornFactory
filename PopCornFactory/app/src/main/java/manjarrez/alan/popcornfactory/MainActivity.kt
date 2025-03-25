cdbaa4aa83f8cf04052f2055f0fdede852410d57
AlanManjarrez <160210523+AlanManjarrez@users.noreply.github.com>
2025-03-24 22:24:47 -0700

    Primera parte del programa

---
+++ PopCornFactory/app/src/main/java/manjarrez/alan/popcornfactory/MainActivity.kt
@@ -0,0 +1,40 @@
+package manjarrez.alan.popcornfactory
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        var boton: Button = findViewById<Button>(R.id.btnCatalogo) as Button
+        boton.setOnClickListener {
+            var intent: Intent = Intent(this, Catalogo::class.java)
+            startActivity(intent)
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
---
+++ PopCornFactory/app/src/main/java/manjarrez/alan/popcornfactory/Pelicula.kt
@@ -0,0 +1,8 @@
+package manjarrez.alan.popcornfactory
+
+data class Pelicula (var titulo: String,
+                     var image: Int,
+                     var header: Int,
+                     var sinopsis: String
+){
+}
